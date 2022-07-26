f57f3b5bd39bb13900bbc0a6aab4ec9d1738f0ee
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2022-07-26 13:58:44 -0400

    removed ID output

--- update_repo.sh
+++ update_repo.sh
@@ -1,6 +1,6 @@
 echo Running Databricks repo update
 databricks configure --host $DATABRICKS_HOST --aad-token
 
-ID=$(databricks repos update --repo-id $(($DATABRICKS_REPO_ID)) --branch $(($DATABRICKS_REPO_BRANCH)) | jq '.id')
+databricks repos update --repo-id $(($DATABRICKS_REPO_ID)) --branch $(($DATABRICKS_REPO_BRANCH))
 
-echo Update Sent, ID: $ID
+echo Done
