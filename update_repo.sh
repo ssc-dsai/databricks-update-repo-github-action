df3353e124fdb840d6cde0bf73cd48c8b4a38d70
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2022-07-28 11:13:48 -0400

    testing new command

--- update_repo.sh
+++ update_repo.sh
@@ -24,16 +24,11 @@ if [[ -z $DATABRICKS_REPO_BRANCH ]] ; then
 fi
 
 databricks configure --host $DATABRICKS_HOST --aad-token
-databricks repos update --repo-id $DATABRICKS_REPO_ID --branch $DATABRICKS_REPO_BRANCH
+ID=$(databricks repos update --repo-id $(($DATABRICKS_REPO_ID)) --branch $(($DATABRICKS_REPO_BRANCH))| jq '.id')
 
+if [[ -z $ID ]]; then
+    echo "Could not update repository"
+    exit 1
+fi
 
-# RESULT=$(databricks repos update --repo-id $(($DATABRICKS_REPO_ID)) --branch $(($DATABRICKS_REPO_BRANCH)))
-
-# echo $RESULT
-
-# if [[ -z $ID ]]; then
-#     echo "Could not update repository"
-#     exit 1
-# fi
-
-# echo Update Sent, Repo ID: $ID
+echo Update Sent
